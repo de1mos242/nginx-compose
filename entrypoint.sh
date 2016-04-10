c0c32dde8defbc1e427a8f2e687a642c0db54d2f
De1mos <de1m0s242@gmail.com>
2016-04-11 03:11:36 +0700

    1.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 
 echo 'Run entrypoint'
 envsubst '$NGINX_HOST $NGINX_PORT $NGINX_PROXY_HOST $NGINX_PROXY_PORT $NGINX_STATIC_FOLDER' < /etc/nginx/conf.d/tomcat-webapp.template > /etc/nginx/conf.d/default.conf
 echo 'Entrypoint refreshed'
 echo 'Nginx result conf:'
 cat /etc/nginx/conf.d/default.conf
+
+exec "$@"
