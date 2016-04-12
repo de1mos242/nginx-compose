c1e5d632257faab68c45c0a2a61c0254c47c0257
De1mos <de1m0s242@gmail.com>
2016-04-12 23:30:32 +0700

    externalize project dependent parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo 'Run entrypoint'
-envsubst '$NGINX_HOST $NGINX_PORT $NGINX_PROXY_HOST $NGINX_PROXY_PORT $NGINX_STATIC_FOLDER' < /etc/nginx/conf.d/tomcat-webapp.template > /etc/nginx/conf.d/default.conf
+envsubst '$NGINX_HOST $NGINX_PORT $NGINX_PROXY_HOST $NGINX_PROXY_PORT $NGINX_STATIC_FOLDER $NGINX_PROXY_LOCATION $NGINX_PROXY_PASS_LOCATION $NGINX_REWRITE_BASE_HEADER' < /etc/nginx/conf.d/tomcat-webapp.template > /etc/nginx/conf.d/default.conf
 echo 'Entrypoint refreshed'
 echo 'Nginx result conf:'
 cat /etc/nginx/conf.d/default.conf
